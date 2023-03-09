3263843b4ad54e3d49332f5d2c1dfdd9b4d2a7fa
harryprayiv <harryprayiv@gmail.com>
2023-03-09 18:29:27 -0500

    Haskell is very elegant

--- src/keypadLogicV3.hs
+++ src/keypadLogicV3.hs
@@ -13,31 +13,27 @@ mkDMXSel xs
   | length xs == 512 = Just $ DMXSel $ listArray (0,511) xs
   | otherwise = Nothing
 
--- Create a valid DMX array
-Just myDMX = mkDMX [0..511]
--- Create a valid DMXSel array
-Just myDMXSel = mkDMXSel $ replicate 512 True
-
--- Create an invalid DMX array
-myInvalidDMX = mkDMX [0..511] ++ [999]
--- Create an invalid DMXSel array
-myInvalidDMXSel = mkDMXSel $ replicate 511 True
-
--- Use pattern matching to extract the underlying array from a DMX value
-DMX arr = myDMX
--- Use pattern matching to extract the underlying array from a DMXSel value
-DMXSel arr = myDMXSel
-
 
 modDmxChVals :: DMX -> DMXSel -> Int -> DMX
 modDmxChVals (DMX d) (DMXSel s) v =
   let d' = array (bounds d) [(i, if s ! i then v else d ! i) | i <- range (bounds d)]
   in DMX d'
 
-
 -- Create a valid DMX array and DMXSel array
 Just myDMX = mkDMX [0..511]
-Just myDMXSel = mkDMXSel $ take 256 $ cycle [True, False]
+
+Just myDMXSel = mkDMXSel $ replicate 512 True
+
+-- Create an invalid DMX array and an invalid DMXSel array
+myInvalidDMX = mkDMX [0..511] ++ [999]
+
+myInvalidDMXSel = mkDMXSel $ replicate 511 True
+
+-- Use pattern matching to extract the underlying array from a DMX value 
+-- then extract the underlying array from a DMXSel value
+DMX arr = myDMX
+
+DMXSel arr = myDMXSel
 
 -- Modify the DMX array using modDmxChVals
 let myModifiedDMX = modDmxChVals myDMX myDMXSel 42
