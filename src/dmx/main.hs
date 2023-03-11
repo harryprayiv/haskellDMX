3cdd6e27b66c9df5fbb08da7203bde8580dbe84f
harryprayiv <harryprayiv@gmail.com>
2023-03-11 16:25:54 -0500

    cleanup

--- src/dmx/main.hs
+++ src/dmx/main.hs
@@ -8,6 +8,3 @@ main = do
   print $ unDmxUniverse universeB
   randomSelection <- randomDmxUniverseBool
   print $ unDmxUniverseBool randomSelection
-
--- ``` 
--- modify main section to print out the values of universeA then print out the values of universeB then print out the values of randomSelection for debugging purposes
