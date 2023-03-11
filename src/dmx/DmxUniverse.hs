e3eb95cd3d559a31039e5b0a767050dca3da1994
harryprayiv <harryprayiv@gmail.com>
2023-03-11 17:16:01 -0500

    modules on day one!

--- src/dmx/DmxUniverse.hs
+++ src/dmx/DmxUniverse.hs
@@ -8,7 +8,9 @@ module DmxUniverse (
     randomDmxUniverse,
     randomDmxUniverseBool,
     newDmxUniverse,
-    setDmxChVals
+    setDmxChVals,
+    mathOps,
+    mapDmxUniverses
 ) where
 
 import System.Random
@@ -50,3 +52,16 @@ setDmxChVals :: DmxUniverse -> DmxUniverseBool -> Word8 -> DmxUniverse
 setDmxChVals (DmxUniverse chVals) (DmxUniverseBool chSel) v =
   let newChVals = zipWith (\sel val -> if sel then v else val) chSel chVals
   in DmxUniverse newChVals
+
+-- Define mathOps function for mathematical operations
+-- mathOps :: Char -> Word8 -> Word8 -> Word8
+-- mathOps '+' a b = max 255 (a + b)
+-- mathOps '-' a b = min 0 (a - b)
+-- mathOps '*' a b = max 255 (a * b)
+-- mathOps '/' _ 0 = 0
+-- mathOps '/' a b = a `div` b
+
+-- -- Define mapDmxUniverses function to apply mathematical operation
+-- mapDmxUniverses :: DmxUniverse -> Char -> DmxUniverse -> DmxUniverse
+-- mapDmxUniverses (DmxUniverse a) op (DmxUniverse b) =
+--   DmxUniverse $ zipWith (mathOps op) a b
--- src/dmx/main.hs
+++ src/dmx/main.hs
@@ -1,10 +1,41 @@
-import DmxUniverse
+module Main where
+
+import System.Random
+import Data.Word
+import DmxUniverse -- assuming your module name is DmxUniverse.hs
 
 main :: IO ()
 main = do
+  -- Create a random DmxUniverse and print its values
   universeA <- randomDmxUniverse
-  print $ unDmxUniverse universeA
+  -- putStrLn $ "Random universe: " ++ show (unDmxUniverse universeA)
+
+  -- Create an empty DmxUniverse and print its values
   let universeB = emptyDmxUniverse
-  print $ unDmxUniverse universeB
+  -- putStrLn $ "Empty universe: " ++ show (unDmxUniverse universeB)
+
+  -- Create a random DmxUniverseBool and print its values
   randomSelection <- randomDmxUniverseBool
-  print $ unDmxUniverseBool randomSelection
+  -- putStrLn $ "Random selection: " ++ show (unDmxUniverseBool randomSelection)
+
+  -- Test the setDmxChVals function
+  let chSel = DmxUniverseBool [True, False, True, False, False, True]
+      chVals = DmxUniverse [100, 20, 30, 254, 50, 60]
+      newVals = setDmxChVals chVals chSel 255
+  putStrLn $ ""
+  putStrLn $ ""
+  putStrLn $ ""
+  putStrLn $ "Testing!"
+
+  putStrLn $ "Old values: " ++ show (unDmxUniverse chVals)
+  putStrLn $ "Selection: " ++ show (unDmxUniverseBool chSel)
+  putStrLn $ "New values: " ++ show (unDmxUniverse newVals)
+
+  -- let uvOne = DmxUniverse [1, 0, 2, 3]
+  --     uvTwo = DmxUniverse [253, 121, 64, 32]
+  --     result = mapDmxUniverses uvOne '+' uvTwo
+  -- putStrLn $ "UV One: " ++ show (unDmxUniverse uvOne)
+  -- putStrLn $ "UV Two: " ++ show (unDmxUniverse uvTwo)
+  -- putStrLn $ "Result: " ++ show (unDmxUniverse result)
+  -- Result should contains: DmxUniverse {unDmxUniverse = [255, 0, 255, 62]}
+
