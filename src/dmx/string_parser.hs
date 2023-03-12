a13d561e1742141fa6eaa49832752f4774cd8540
harryprayiv <harryprayiv@gmail.com>
2023-03-11 23:29:06 -0500

    string parser turned off

--- src/dmx/string_parser.hs
+++ src/dmx/string_parser.hs
@@ -1,6 +1,7 @@
 import Text.Regex.Posix
 import Data.Word
 import DmxUniverse
+import DmxUniverseBuilders 
 
 type Command = (Int, Int, Word8, String) -- start index, end index, value, value type
 
@@ -11,20 +12,14 @@ parseCommand str =
     [[_, i, v]] -> Just (read i, read i, round (read v / 100 * 255), "percentage")
     _ -> Nothing
 
-createCommandOutput :: Command -> DmxUniverse
-createCommandOutput (start, end, value, "percentage") =
-  let universe = emptyDmxUniverse in
-  setDmxChVals universe (DmxUniverse $ replicate start False ++ replicate (end-start+1) True ++ replicate (512-end) False) value
-createCommandOutput (start, end, value, "absolute") =
-  let universe = emptyDmxUniverse in
-  setDmxChVals universe (DmxUniverseBool $ replicate start False ++ replicate (end-start+1) True ++ replicate (512-end) False value)
+-- createCommandOutput :: Command -> DmxUniverse
+-- createCommandOutput (start, end, value, "percentage") =
+--   let universe = emptyDmxUniverse in
+--   setDmxChVals universe (DmxUniverse $ replicate start False ++ replicate (end-start+1) True ++ replicate (512-end) False) value
+-- createCommandOutput (start, end, value, "absolute") =
+--   let universe = emptyDmxUniverse in
+--   setDmxChVals universe (DmxUniverseBool $ replicate start False ++ replicate (end-start+1) True ++ replicate (512-end) False) value
 
--- Example usage
+-- -- Example usage
 main :: IO ()
-main = do
-  let commandStr = "1 through 512 at 100%"
-      maybeCommand = parseCommand commandStr
-      maybeOutput = maybeCommand >>= return . createCommandOutput
-  case maybeOutput of
-    Just output -> putStrLn $ show (unDmxUniverse output)
-    Nothing -> putStrLn "Invalid command"
+main = do putStrLn "Invalid command"
